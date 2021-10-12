495361d29806c2a35bbb883d71a6a2d058979773
nnnnusui <nnnnusui@gmail.com>
2021-10-13 05:48:13 +0900

    add: Service memo

--- src/main/scala/com/github/nnnnusui/historizer/Service.scala
+++ src/main/scala/com/github/nnnnusui/historizer/Service.scala
@@ -3,7 +3,35 @@ package com.github.nnnnusui.historizer
 import zio.{Has, Ref, ZIO, ZLayer}
 
 object Service {
-  trait Service {}
+  trait Service {
+    // ID = Int | ULID
+    // Identified[T] = (ID, T)
+    // Content(parentId: Content.ID)
+
+    // Article: Content (id == parentId)
+    def findArticles: UIO[Seq[Article]] // () => article.findAll
+    def findArticle()                   // ID => article.findBy
+    def addArticle()                    // Article => article.create
+    def updateArticle()                 // Identified[Article] => article.update
+    def publishArticle()                // ID => article.update
+
+    // Content (id != parentId)
+    def findContent()   // ID => content.findBy
+    def addContent()    // Content => content.create => history.create
+    def updateContent() // Identified[Content] => content.update => history.create
+
+    // Text(contentId: ID, value: String)
+    def addText()           // Text => text.create
+    def addPartialText()    // PartialText => text.update => history.create
+    def removePartialText() // PartialText => text.update => history.create
+
+    // History(contentId: Content.ID, undoAction: Action, timestamp: Timestamp, commitId: ID)
+    def findHistories() // Article.Key => history.findBy
+    def findHistory()   // ID => history.findBy
+
+    // Commit(comment: String, timestamp: Timestamp)
+    def commit() // () => commit.create (update latest ID)
+  }
   type Get   = Has[Service]
   type IO[A] = ZIO[Get, Throwable, A]
 
