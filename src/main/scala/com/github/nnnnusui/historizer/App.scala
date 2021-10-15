bd5b669f0cf3c79623d7785eedc9cb7f4e90b2e1
nnnnusui <nnnnusui@gmail.com>
2021-10-15 09:16:26 +0900

    update: Front-end publish server integrated into back-end server

--- src/main/scala/com/github/nnnnusui/historizer/App.scala
+++ src/main/scala/com/github/nnnnusui/historizer/App.scala
@@ -3,10 +3,13 @@ package com.github.nnnnusui.historizer
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import caliban.interop.circe.AkkaHttpCirceAdapter
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
 import com.github.nnnnusui.historizer.controller.Api
+import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.sslconfig.util.ConfigLoader
 import zio.Runtime
 import zio.clock.Clock
 import zio.console.Console
@@ -25,13 +28,17 @@ object App extends App with AkkaHttpCirceAdapter {
 
   val interpreter = runtime.unsafeRun(Api.api.interpreter)
 
-  val route = cors() {
-    path("api" / "graphql") {
-      adapter.makeHttpService(interpreter)
-    } ~ path("ws" / "graphql") {
-      adapter.makeWebSocketService(interpreter)
+  val publishDir = ConfigFactory.load().getString("publishDir")
+  val route =
+    cors() {
+      path("api" / "graphql") {
+        adapter.makeHttpService(interpreter)
+      } ~ path("ws" / "graphql") {
+        adapter.makeWebSocketService(interpreter)
+      } ~ pathEndOrSingleSlash {
+        getFromFile(s"$publishDir/index.html")
+      } ~ getFromDirectory(publishDir)
     }
-  }
 
   val bindingFuture = Http().newServerAt("localhost", 8088).bind(route)
 
